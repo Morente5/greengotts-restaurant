375dd9bc6d1533cf825083b6529557f30b4264f3
Alejandro Morente <morente5@hotmail.es>
2017-02-14 17:14:24 +0100

    Update 2017-02-14T16:14:21.125Z

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(".owl-carousel").owlCarousel({items:1,loop:!0,margin:10,autoplay:!0,autoplayTimeout:3e3,autoplayHoverPause:!0,responsive:{0:{items:1},744:{items:2},992:{items:4}}})});
