4d772984db5836de8bca3c3f712889c64deeb876
Alejandro Morente <morente5@hotmail.es>
2017-02-15 21:28:59 +0100

    Added map and favicon

---
+++ app/scripts/location.js
@@ -0,0 +1,22 @@
+function initMap(lat, lng) {
+    const divMap = document.getElementById("map");
+    const initPosition = new google.maps.LatLng(28.541455, -81.375923);
+    const coords = [initPosition];
+    
+    const map = new google.maps.Map(divMap, {
+        center: initPosition, 
+        zoom: 16,
+        disableDefaultUI: true
+    });
+
+    const marker = new google.maps.Marker({
+        position: initPosition,
+        title: '#' + coords.length,
+        map: map,
+        icon: 'resources/marker.png',
+        animation: google.maps.Animation.DROP
+    });
+}
+
+
+initMap(37.171204, -3.598550);
