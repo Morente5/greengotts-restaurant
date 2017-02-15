17acc1dadb0c7f0be4f1bc6bb4241c0ded681522
Alejandro Morente <morente5@hotmail.es>
2017-02-15 03:07:49 +0100

    Finished menu, form and opinions

--- app/scripts/gallery.js
+++ app/scripts/gallery.js
@@ -8,16 +8,36 @@ $(document).ready(function() {
     autoplayTimeout: 4000,
     autoplayHoverPause: true,
     responsive:{
-        0: {
-            items: 1
-        },
-        744: {
-            items: 2
-        },
-        992: {
-            items: 3
-        }
+      0: {
+          items: 1
+      },
+      744: {
+          items: 2
+      },
+      992: {
+          items: 3
+      }
     },
   });
+
+  updateSize();
  
 });
+
+function updateSize(){
+  var minHeight = parseInt($('.owl-item').eq(0).css('height'));
+  $('.owl-item').each(function () {
+    var thisHeight = parseInt($(this).css('height'));
+    minHeight = (minHeight <= thisHeight ? minHeight : thisHeight);
+  });
+  $('.owl-wrapper-outer').css('height',minHeight+'px');
+
+  $('.owl-carousel .owl-item img').each(function(){
+    var thisHeight = parseInt($(this).css('height'));
+    if(thisHeight > minHeight){
+
+      $(this).css('margin-top', -1 * (thisHeight - minHeight) / 2 + 'px');
+      $(this).css('margin-bottom', -1 * (thisHeight - minHeight) / 2 + 'px');
+    }
+  });
+}
