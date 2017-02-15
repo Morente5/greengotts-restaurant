de95b90425296ef1f55e5a9a983efcd3efbb2331
Alejandro Morente <morente5@hotmail.es>
2017-02-15 20:47:07 +0100

    Commented img modal

--- app/scripts/img-modal.js
+++ app/scripts/img-modal.js
@@ -4,17 +4,17 @@ only when clicked and not dragged (drag to slide the gallery)
 
 var isDragging = false;
 
-$("#owl-carousel img")
+$('#owl-carousel img')
 .mousedown(function() {
     $(window).mousemove(function() {
         isDragging = true;
-        $(window).unbind("mousemove");
+        $(window).unbind('mousemove');
     });
 })
 .mouseup(function() {
     var wasDragging = isDragging;
     isDragging = false;
-    $(window).unbind("mousemove");
+    $(window).unbind('mousemove');
     if (!wasDragging) {  // If not dragging
         $('#galleryImage').attr('src', $(this).attr('src'));  // Asign the image source to the modal
    		$('#galleryModal').modal('show');  // and show the modal
