9315764b7a7759d1ff180e05f8e58300aa2f3c6d
Alejandro Morente <morente5@hotmail.es>
2017-02-15 18:43:43 +0100

    Added Title font, favicon, fixed colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,14 +8,14 @@ new WOW().init();
 $('a.page-scroll').bind('click', function(event) {
     var $ele = $(this);
     $('html, body').stop().animate({
-        scrollTop: ($($ele.attr('href')).offset().top - 60)
+        scrollTop: ($($ele.attr('href')).offset().top - 45)
     }, 1450, 'easeInOutExpo');
     event.preventDefault();
 });
 
 $('#collapsingNavbar li a').click(function() {
     /* always close responsive nav after click */
-    $('.navbar-toggler:visible').click();
+    $('.navbar-toggler:not(.collapsed)').click();
 });
 
 $('#galleryModal').on('show.bs.modal', function (e) {
