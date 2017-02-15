c33365118eecc2bf3516997e3a29df3e6ca1eccd
Alejandro Morente <morente5@hotmail.es>
2017-02-15 19:05:15 +0100

    Image preview modal

---
+++ app/scripts/img-modal.js
@@ -0,0 +1,22 @@
+/* Allows showing the gallery image preview modal
+only when clicked and not dragged (drag to slide the gallery)
+*/
+
+var isDragging = false;
+
+$("#owl-carousel img")
+.mousedown(function() {
+    $(window).mousemove(function() {
+        isDragging = true;
+        $(window).unbind("mousemove");
+    });
+})
+.mouseup(function() {
+    var wasDragging = isDragging;
+    isDragging = false;
+    $(window).unbind("mousemove");
+    if (!wasDragging) {  // If not dragging
+        $('#galleryImage').attr('src', $(this).attr('src'));  // Asign the image source to the modal
+   		$('#galleryModal').modal('show');  // and show the modal
+    }
+});
